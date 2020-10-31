76d50062eea77ba216444c23abfe86755c7aba47
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-10-31 15:30:57 -0400

    restricted write access to printf to one writer using a Mutex

--- src/main.cpp
+++ src/main.cpp
@@ -13,28 +13,37 @@ DigitalIn button(USER_BUTTON);
 DigitalOut rx_led(LED2);
 DigitalOut tx_led(LED3);
 
-rtos::Thread send_thread;
-rtos::Thread recv_thread;
+Thread send_thread;
+Thread recv_thread;
+
+Mutex printMutex;
 
 void send_can_message(string message) {
     CANMessage msg(1, message.c_str(), message.length() + 1);
     tx_led = 1;
     if (can1.write(msg)) {
+        printMutex.lock();
         printf("[CAN1] Sent CAN message '%s'\n\r", message.c_str());
+        printMutex.unlock();
     } else {
+        printMutex.lock();
         printf("[CAN1] Failed to send CAN message '%s'\n\r", message.c_str());
+        printMutex.unlock();
     }
     tx_led = 0;
-    
 }
 
 void recv_func() {
     CANMessage message;
+    printMutex.lock();
     printf("[CAN2] Starting thread to receive CAN messages...\n\r");
+    printMutex.unlock();
     while (1) {
         while (can2.read(message)) {
             rx_led = 1;
+            printMutex.lock();
             printf("[CAN2] '%s'\n\r", message.data);
+            printMutex.unlock();
         }
         rx_led = 0;
     }
@@ -44,11 +53,15 @@ void read_from_serial() {
     string buffer = "";
     list<string> messages;
     char c;
+    printMutex.lock();
     printf("[CAN1] Starting thread to listening for input...\n\r");
+    printMutex.unlock();
     while (1) {
         while (1) {
             device.read(&c, 1);
+            printMutex.lock();
             printf("%c", c);
+            printMutex.unlock();
             if (c == '\r') {
                 messages.push_back(buffer);
                 break;
@@ -61,14 +74,18 @@ void read_from_serial() {
                 buffer = "";
             }
         }
+        printMutex.lock();
         printf("\n\r");
+        printMutex.unlock();
         while (!messages.empty()) {
             send_can_message(messages.front());
             messages.pop_front();
         }
         while (device.readable()) {
             device.read(&c, 1);
+            printMutex.lock();
             printf("%c\n\r", c);
+            printMutex.unlock();
         }
     }
 
