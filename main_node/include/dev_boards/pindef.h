eefcf30b36138aa09a5ee182fd0ae19fb2fdbcba
William Zhang <willzhang05@gmail.com>
2020-11-06 00:06:48 -0500

    reorganize repo

---
+++ main_node/include/dev_boards/pindef.h
@@ -0,0 +1,17 @@
+#ifndef PINDEF_H
+#define PINDEF_H
+
+/***************************/
+/* pin definitions for CAN */
+/***************************/
+#define CAN1_RX      PD_0   // Rx for MCU
+#define CAN1_TX      PD_1
+#define CAN2_RX      PB_5   // Rx for MCU
+#define CAN2_TX      PB_6
+
+#define CELL_VOLTAGE  PA_0
+#define BALANCING_CONTROL   PA_4
+
+#define FAN_CTRL                    PB_4
+#define FAN_PWM                     PB_5
+#endif // PINDEF_H
