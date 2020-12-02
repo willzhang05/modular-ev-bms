383eef47d85adbebe8330bd9bf02a2a87d62b965
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-12-01 19:34:56 -0500

    main_node: add TESTING and PRINTING macros

--- main_node/src/main.cpp
+++ main_node/src/main.cpp
@@ -1,6 +1,9 @@
 #include <mbed.h>
 #include <pindef.h>
 
+// #define TESTING     // only defined if using test functions
+#define PRINTING    // only defined if using printf functions
+
 BufferedSerial device(USBTX, USBRX);
 
 AnalogIn pack_volt(PACK_VOLTAGE);
@@ -31,34 +34,47 @@ int current_length = 7;
 float c_voltage_output [7] = {0, 0.5, 1, 2.5, 4, 4.5, 5};
 float current [7] = {-2700, -1300, -700, 30, 1300, 1500, 3300};
 
+#ifdef TESTING
 bool test_pack_voltage(float test_min, float test_max){
     float v = pack_volt.read();
+#ifdef PRINTING
     int v_int = (int)v;
     int v_dec = (int)(v*100);
     printf("%d.%d\n\r", v_int, v_dec);
+#endif //PRINTING
     if(v>=test_min && v<=test_max){
+#ifdef PRINTING
         printf("Pack Voltage Test PASSED \n\r");
+#endif //PRINTING
         return true;
     }
     else
     {
+#ifdef PRINTING
         printf("Pack Voltage Test FAILED \n\r");
+#endif //PRINTING
         return false; 
     }
 }
 
 bool test_pack_current(float test_min, float test_max){
     float i = pack_current.read();
+#ifdef PRINTING
     int i_int = (int)i;
     int i_dec = (int)(i*100);
     printf("%d.%d\n\r", i_int, i_dec);
+#endif //PRINTING
     if(i>=test_min && i<=test_max){
+#ifdef PRINTING
         printf("Pack Current Test PASSED \n\r");
+#endif //PRINTING
         return true;
     }
     else
     {
+#ifdef PRINTING
         printf("Pack Current Test FAILED \n\r");
+#endif //PRINTING
         return false; 
     }
 }
@@ -66,16 +82,22 @@ bool test_pack_current(float test_min, float test_max){
 bool test_fan_output(){
     char c;
     fan_ctrl.write(0);
-    fan_pwm.write(0.0); 
+    fan_pwm.write(0.0);
+#ifdef PRINTING
     printf("Fan ctrl set to Low, press any key to continue...  \n\r");
+#endif //PRINTING
     device.read(&c, 1);
     fan_ctrl.write(1);
     fan_pwm.write(0.1);
+#ifdef PRINTING
     printf("Fan ctrl set to High, PWM set to Low, press any key to continue...  \n\r");
+#endif //PRINTING
     device.read(&c, 1);
     fan_ctrl.write(1);
     fan_pwm.write(0.9);
+#ifdef PRINTING
     printf("Fan ctrl set to High, PWM set to High, Did test pass (y/n)? \n\r");
+#endif //PRINTING
     device.read(&c, 1);
     fan_ctrl.write(0);
     fan_pwm.write(0.0);
@@ -88,13 +110,17 @@ bool test_fan_output(){
 
 bool test_discharge_contactor(){
     discharge_contactor.write(0); // switch balancing off
+#ifdef PRINTING
     printf("Discharge Contactor Output set to Low, measure current and then press any key to continue... \n\r");
+#endif //PRINTING
     char c;
     //while(1)
     //{
     device.read(&c, 1);
     discharge_contactor.write(1); // switch balancing on
+#ifdef PRINTING
     printf("Discharge Contactor Output set to High, measure current. Did test pass (y/n)? \n\r");
+#endif //PRINTING
     device.read(&c, 1);
     discharge_contactor.write(0); // switch balancing off
     if(c=='y'){
@@ -106,13 +132,17 @@ bool test_discharge_contactor(){
 
 bool test_charge_contactor(){
     charge_contactor.write(0); // switch balancing off
+#ifdef PRINTING
     printf("Charge Contactor Output set to Low, measure current and then press any key to continue... \n\r");
+#endif //PRINTING
     char c;
     //while(1)
     //{
     device.read(&c, 1);
     charge_contactor.write(1); // switch balancing on
+#ifdef PRINTING
     printf("Ccharge Contactor Output set to High, measure current. Did test pass (y/n)? \n\r");
+#endif //PRINTING
     device.read(&c, 1);
     charge_contactor.write(0); // switch balancing off
     if(c=='y'){
@@ -121,14 +151,19 @@ bool test_charge_contactor(){
 
     return false;
 }
+
 void test_sleep()
 {
     char c;
+#ifdef PRINTING
     printf("Board not sleeping right now, press any key to go to sleep...  \n\r");
+#endif //PRINTING
     device.read(&c, 1);
     sleep();
 
 }
+#endif //TESTING
+
 void cell_balancing_logic(){ //Cell balancing based on voltage
     if(cell_num>0){
         float min_cell_voltage = cell_voltages[0];
@@ -196,15 +231,23 @@ float get_cell_voltage(){
 }
 int main() {
     // device.set_baud(38400);
-    printf("test start \n\r");
+#ifdef PRINTING
+    printf("start main() \n\r");
+#endif //PRINTING
 
     while(1){
+#ifdef TESTING
         test_point_0 = test_point_0 ^ 1;
+#ifdef PRINTING
         printf("writing \n\r");
+#endif //PRINTING
         test_pack_voltage(0, 1);
         test_pack_current(0, 1);
         // test_fan_output();
         thread_sleep_for(1000);
+#ifdef PRINTING
         printf("\r\n");
+#endif //PRINTING
+#endif //TESTING
     }
 }
