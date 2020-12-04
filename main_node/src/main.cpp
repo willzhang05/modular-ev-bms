909c2a0fe87888d89f62cc342c43d99cbc0081a5
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-12-04 00:22:43 -0500

    main_node: clean up code

--- main_node/src/main.cpp
+++ main_node/src/main.cpp
@@ -5,6 +5,8 @@
 #define PRINTING    // only defined if using printf functions
 
 #define NUM_ADC_SAMPLES 10
+#define NUM_CELL_NODES  2
+// #define VDD 3.3f
 
 #ifdef PRINTING
 #include "Printing.h"
@@ -29,17 +31,10 @@ DigitalOut extCanStby(EXT_CAN_STBY);
 
 Ticker intCanTxTicker;
 
-int cell_num = 2;
-int max_cell_node = 50;
-float cell_voltages [50];
+float cell_voltages[NUM_CELL_NODES];
 float cell_balancing_thresh = 0.3f;
-float cell_temperatures [50];
+float cell_temperatures [NUM_CELL_NODES];
 float temperature_thresh = 30.0f;
-float VDD = 3.3f;
-
-int current_length = 7;
-float c_voltage_output [7] = {0, 0.5, 1, 2.5, 4, 4.5, 5};
-float current [7] = {-2700, -1300, -700, 30, 1300, 1500, 3300};
 
 #ifdef TESTING
 DigitalOut test_point_0(UNUSED_PIN_0);
@@ -176,56 +171,51 @@ void test_sleep()
 #endif //TESTING
 
 void cell_balancing_logic(){ //Cell balancing based on voltage
-    if(cell_num>0){
-        float min_cell_voltage = cell_voltages[0];
-        for(int i = 0;i<cell_num;i++ )
-        {
-            if(cell_voltages[i]<min_cell_voltage){
-                min_cell_voltage = cell_voltages[i];
-            }
-        }
-        for(int i = 0;i<cell_num;i++ )
-        {
-            if(cell_voltages[i]>(min_cell_voltage+cell_balancing_thresh)){
-                //Turn cell balancing on
-            }
-            else{
-                //turn cell balancing off
-            }
+    float min_cell_voltage = cell_voltages[0];
+    for(int i = 0; i < NUM_CELL_NODES; ++i)
+    {
+        if(cell_voltages[i] < min_cell_voltage){
+            min_cell_voltage = cell_voltages[i];
         }
     }
-    
+    for(int i = 0; i < NUM_CELL_NODES; ++i)
+    {
+        if(cell_voltages[i] > (min_cell_voltage+cell_balancing_thresh)){
+            //Turn cell balancing on
+        }
+        else{
+            //turn cell balancing off
+        }
+    }
 }
 
 void fan_logic(){
-    if(cell_num>0){
-        bool fanOn = false;
-        float max_cell_temp = cell_temperatures[0];
-        for(int i = 0;i<cell_num;i++ )
-        {
-            if(cell_temperatures[i]>temperature_thresh){
-                fanOn = true;
-            }
-            if(cell_temperatures[i]>max_cell_temp){
-                max_cell_temp = cell_temperatures[i];
-            }
+    bool fanOn = false;
+    float max_cell_temp = cell_temperatures[0];
+    for(int i = 0; i < NUM_CELL_NODES; ++i)
+    {
+        if(cell_temperatures[i] > temperature_thresh){
+            fanOn = true;
         }
-        if(fanOn)
-        {
-            fan_ctrl.write(1);
-            float fan_power = (max_cell_temp-max_cell_temp)/20; 
-            if(fan_power>1.0){
-                fan_power = 1.0;
-            }
-            else if(fan_power<0.0){
-                fan_power = 0.0;
-            }
-            fan_pwm.write(fan_power);
+        if(cell_temperatures[i] > max_cell_temp){
+            max_cell_temp = cell_temperatures[i];
         }
-        else{
-            fan_ctrl.write(0);
-            fan_pwm.write(0.0); 
+    }
+    if(fanOn)
+    {
+        fan_ctrl.write(1);
+        float fan_power = (max_cell_temp-max_cell_temp)/20; 
+        if(fan_power > 1.0){
+            fan_power = 1.0;
         }
+        else if(fan_power < 0.0){
+            fan_power = 0.0;
+        }
+        fan_pwm.write(fan_power);
+    }
+    else{
+        fan_ctrl.write(0);
+        fan_pwm.write(0.0); 
     }
 }
 
@@ -282,16 +272,6 @@ float get_pack_current()
     return i;
 }
 
-float get_cell_voltage(){
-    float c_voltage = pack_current.read()*VDD;
-    for(int i =0;i<current_length-1;i++){ //Voltage calibration
-        if(c_voltage>=c_voltage_output[i] && c_voltage<=c_voltage_output[i+1]){
-            return ((c_voltage-c_voltage_output[i])*(current[i+1]-current[i])/(c_voltage_output[i+1]-c_voltage_output[i]))+current[i]; //SOC based on voltage calibration
-        }
-    }
-    return -2800.0;
-}
-
 // WARNING: This method is NOT safe to call in an ISR context (if RTOS is enabled)
 // This method is Thread safe (CAN is Thread safe)
 bool sendCANMessage(const char *data, const unsigned char len = 8)
@@ -328,16 +308,16 @@ void canRxIrqHandler()
 
 void canInit()
 {
-    intCanTxTicker.attach(&canTxIrqHandler, 1); // float, in seconds
+    intCanTxTicker.attach(&canTxIrqHandler, 1s);
     intCan.attach(&canRxIrqHandler, CAN::RxIrq);
     intCanStby = 0;
 }
 
 int main() {
     // This is only necessary if using software reset (not NRST pin)
-    HAL_DBGMCU_DisableDBGSleepMode();
-    HAL_DBGMCU_DisableDBGStandbyMode();
-    HAL_DBGMCU_DisableDBGStopMode();
+    HAL_DBGMCU_EnableDBGSleepMode();
+    HAL_DBGMCU_EnableDBGStandbyMode();
+    HAL_DBGMCU_EnableDBGStopMode();
 
     // device.set_baud(38400);
 #ifdef PRINTING
