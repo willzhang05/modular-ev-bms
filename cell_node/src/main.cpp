79bc5f753dc169617463f48e65d21fe59a78f0e7
Nripesh Manandhar <nm6wbc@virginia.edu>
2020-12-05 17:37:34 -0500

    cell_node: change all printf's to PRINT's

--- cell_node/src/main.cpp
+++ cell_node/src/main.cpp
@@ -1,13 +1,9 @@
 #include <mbed.h>
 #include "pindef.h"
 #include "CANStructs.h"
+#include "Printing.h"
 
 // #define TESTING // only defined when using test functions
-#define PRINTING // only defined when using printf functions
-
-#ifdef PRINTING
-#include "Printing.h"
-#endif //PRINTING
 
 // This uses a lot of ROM!!!
 // BufferedSerial device(USBTX, USBRX, 38400);
@@ -183,33 +179,33 @@ int8_t current_cell_temp;  // -40degC to +80degC, units of 1degC
 
 #ifdef TESTING
 bool test_cell_voltage(uint16_t test_min, uint16_t test_max){
-    printf("start test_cell_voltage()\r\n");
+    PRINT("start test_cell_voltage()\r\n");
     float v = cell_volt.read();
     int v_int = (int)v;
     int v_dec = (int)(v*100);
-    printf("Cell Voltage: %d.%d\r\n", v_int, v_dec);
+    PRINT("Cell Voltage: %d.%d\r\n", v_int, v_dec);
 
-    // printf("TEST PRINT after analog in\r\n");
+    // PRINT("TEST PRINT after analog in\r\n");
 
     if((v>=test_min) && (v<=test_max)){
-        printf("Cell Voltage Test PASSED \r\n");
+        PRINT("Cell Voltage Test PASSED \r\n");
         return true;
     }
     else
     {
-        printf("Cell Voltage Test FAILED \r\n");
+        PRINT("Cell Voltage Test FAILED \r\n");
         return false; 
     }
 }
 
 bool test_balance_output(){
     balance_out->write(0); // switch balancing off
-    printf("Balance Output set to Low, measure current and then press any key to continue... \n\r");
+    PRINT("Balance Output set to Low, measure current and then press any key to continue... \n\r");
     // char c;
     // device.read(&c, 1);
     thread_sleep_for(2000);
     balance_out->write(1); // switch balancing on
-    printf("Balance Output set to High, measure current. Did test pass (y/n)? \n\r");
+    PRINT("Balance Output set to High, measure current. Did test pass (y/n)? \n\r");
     // device.read(&c, 1);
     thread_sleep_for(2000);
     balance_out->write(0); // switch balancing off
@@ -224,14 +220,14 @@ bool test_cell_temperature(float test_min, float test_max){
     float t = cell_temp.read();
     int t_int = (int)t;
     int t_dec = (int)(t*100);
-    printf("%d.%d\n\r", t_int, t_dec);
+    PRINT("%d.%d\n\r", t_int, t_dec);
     if(t>=test_min && t<=test_max){
-        printf("Pack Current Test PASSED \n\r");
+        PRINT("Pack Current Test PASSED \n\r");
         return true;
     }
     else
     {
-        printf("Pack Current Test FAILED \n\r");
+        PRINT("Pack Current Test FAILED \n\r");
         return false; 
     }
 }
@@ -239,7 +235,7 @@ bool test_cell_temperature(float test_min, float test_max){
 void test_sleep()
 {
     // char c;
-    printf("Board not sleeping right now, press any key to go to sleep...  \n\r");
+    PRINT("Board not sleeping right now, press any key to go to sleep...  \n\r");
     // device.read(&c, 1);
     sleep();
 
@@ -265,9 +261,7 @@ void canTxIrqHandler() {
     toSend.CellVolt = current_cell_volt;
     toSend.CellTemp = current_cell_temp;
     if (sendCANMessage(GET_CAN_MESSAGE_ID(DEVICE_ID, CELL_DATA_PRIORITY), (char*)&toSend, sizeof(toSend))) {
-#ifdef PRINTING
-        printf("Message sent!\n"); // This should be removed except for testing CAN
-#endif //PRINTING
+        PRINT("Message sent!\n");
     }
 }
 
@@ -276,9 +270,7 @@ void canRxIrqHandler() {
     CANMessage receivedCANMessage;
     while (can1->read(receivedCANMessage)) {
         uint32_t messageID = receivedCANMessage.id;
-#ifdef PRINTING
-        printf("Message received: %s\n", receivedCANMessage.data); // This should be changed to copying the CAN data to a global variable, except for testing CAN
-#endif //PRINTING
+        PRINT("Message received: %s\n", receivedCANMessage.data);
     }
 }
 
@@ -311,12 +303,12 @@ int16_t get_cell_temperature() {
     float t_direct = cell_temp.read();
     float t = T(t_direct * CELL_TEMP_MULT);
     int8_t the_cell_temp = (int8_t)(t);
-#ifdef PRINTING
-    printf("Direct Cell Temperature: ");
+
+    PRINT("Direct Cell Temperature: ");
     printFloat(t_direct, 4);
-    printf("\r\n");
-    printf("Cell Temperature: %d degrees C\r\n", the_cell_temp);
-#endif //PRINTING
+    PRINT("\r\n");
+    PRINT("Cell Temperature: %d degrees C\r\n", the_cell_temp);
+
     return the_cell_temp;
 }
 
@@ -324,14 +316,14 @@ uint16_t get_cell_voltage() {
     float v_direct = cell_volt.read();
     float v = v_direct * CELL_VOLT_MULT;
     uint16_t the_cell_volt = (uint16_t)(v*10000);
-#ifdef PRINTING
-    printf("Direct Cell Voltage: ");
+
+    PRINT("Direct Cell Voltage: ");
     printFloat(v_direct, 4);
-    printf("\r\n");
-    printf("Cell Voltage: ");
+    PRINT("\r\n");
+    PRINT("Cell Voltage: ");
     printIntegerAsFloat(the_cell_volt, 4);
-    printf(" V\r\n");
-#endif //PRINTING
+    PRINT(" V\r\n");
+
     return the_cell_volt;
 }
 //********** End Cell Measurements **********
@@ -347,24 +339,21 @@ int main() {
 
     DigitalOut led2(LED2);
 
-#ifdef PRINTING
-    printf("start of main()\r\n");
-#endif //PRINTING
+    PRINT("start of main()\r\n");
 
     while(1) {
         // do nothing
         led2 = led2 ^ 1;
-#ifdef PRINTING
-        printf("Hello! \r\n");
-#endif //PRINTING
+
+        PRINT("Hello! \r\n");
+
 #ifdef TESTING
         test_cell_voltage(0,1);
 #endif //TESTING
         current_cell_volt = get_cell_voltage();
         current_cell_temp = get_cell_temperature();
-        // thread_sleep_for(1000);
-#ifdef PRINTING
-        printf("\r\n");
-#endif //PRINTING
+        thread_sleep_for(1000);
+
+        PRINT("\r\n");
     }
 }
