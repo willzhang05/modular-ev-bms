92dce35ec7d45ab3aff0e35ea2d10ff2c3a695c3
Nripesh Manandhar <nm6wbc@virginia.edu>
2020-12-04 20:23:31 -0500

    add CAN message structs to shared CANStructs.h

--- include/CANStructs.h
+++ include/CANStructs.h
@@ -22,13 +22,32 @@ typedef struct Balancing {
 // Cell Node -> Main Node message
 // Cell data (analog inputs)
 typedef struct CellData {
-    uint16_t CellVoltage;   // 0V to 6.5535V, units of 0.0001V
-    int8_t CellTemperature; // -40degC to +80degC, units of 1degC
-                            // offset of 20degC so
-                            // if value sent on CAN = -60degC,
-                            // actual cell temperature = -40degC
+    uint16_t CellVolt;  // 0V to 5V, units of 0.0001V
+    int8_t CellTemp;    // -40degC to +80degC, units of 1degC
+                        // Note: temperatures have offset of 20degC so
+                        // if value sent on CAN = -60degC,
+                        // actual cell temperature = -40degC
 } CellData;
 
 //********** END INTERNAL CAN MESSAGES **********
+
+//********** EXTERNAL CAN MESSAGES **********
+
+// Main Node -> External Listener message
+// Battery Pack Status
+// Only pack wide data included, not individual cell data
+typedef struct PackStatus {
+    uint8_t SOC;            // State of charge, 0% to 100%, units 0.5%
+    uint8_t SOH;            // State of health, 0% to 100%, units 0.5%
+    uint16_t PackVolt;      // 0V to 100V, units of 0.01V
+    int16_t PackCurrent;    // -1000A to +1000A, units of 0.1A
+    int8_t PackMaxTemp;     // -40degC to +80degC, units of 1degC
+    int8_t PackAvgTemp;     // -40degC to +80degC, units of 1degC
+                            // Note: temperatures have offset of 20degC so
+                            // if value sent on CAN = -60degC,
+                            // actual cell temperature = -40degC
+} PackStatus;
+
+//********** END EXTERNAL CAN MESSAGES **********
 
 #endif // CANSTRUCTS_H
