5d1c502360db53540c799fb4c87867b0682be180
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-12-05 17:07:59 -0500

    add PRINT macro

--- include/Printing.h
+++ include/Printing.h
@@ -1,6 +1,14 @@
 #ifndef PRINTING_H
 #define PRINTING_H
 
+#define PRINTING    // only defined if using printf functions
+
+#ifdef PRINTING
+#define PRINT(formatstring, ...) printf(formatstring, ##__VA_ARGS__)
+#else  //PRINTING
+#define PRINT(formatstring, ...) do { } while(0)
+#endif //PRINTING
+
 // Input: an integer representing a float with decimals digits past decimal multiplied by 10^decimals
 // Output: print num as a float
 void printIntegerAsFloat(int num, int decimals);
