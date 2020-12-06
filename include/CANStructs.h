99320cd6116f914a8543e77eb5297773033bf295
Nripesh Manandhar <nm6wbc@virginia.edu>
2020-12-05 19:49:22 -0500

    change DEVICE_ID to NODE_ID in CANStructs.h

--- include/CANStructs.h
+++ include/CANStructs.h
@@ -5,10 +5,10 @@
 
 // define any shared structs to be sent over CAN here
 
-// CAN message ID (11 bits): Device ID stored in bottom 7 bits (bits 0-6),
+// CAN message ID (11 bits): Node ID stored in bottom 7 bits (bits 0-6),
 //                           Priority stored in next 4 bits (bits 7-10)
-#define GET_CAN_MESSAGE_ID(DEVICE_ID, PRIORITY) ((DEVICE_ID&0x7F)|((PRIORITY&0x0F)<<7))
-#define GET_DEVICE_ID(CAN_MESSAGE_ID)           (CAN_MESSAGE_ID&0x7F)
+#define GET_CAN_MESSAGE_ID(NODE_ID, PRIORITY)   ((NODE_ID&0x7F)|((PRIORITY&0x0F)<<7))
+#define GET_NODE_ID(CAN_MESSAGE_ID)             (CAN_MESSAGE_ID&0x7F)
 #define GET_PRIORITY(CAN_MESSAGE_ID)            ((CAN_MESSAGE_ID>>7)&0x0F)
 
 //********** INTERNAL CAN MESSAGES **********
